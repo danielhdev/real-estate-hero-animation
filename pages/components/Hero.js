c4e685bc425a49877d4c5b45ec09250ada295077
danielhdev <d.huebschmann@tutanota.com>
2023-01-24 15:06:48 +0100

    1st commit

--- pages/components/Hero.js
+++ pages/components/Hero.js
@@ -1,17 +1,132 @@
 'use client'
 import herostyles from '@/styles/Hero.module.css'
+import { motion } from 'framer-motion'
+
 
 export const Hero = () => {
+    const headline = {
+        initial: {
+            scale: 1.1,
+            x: 40,
+        },
+        animate: {
+            scale: 1,
+            x: 0,
+            transition: {
+                duration: 4,
+                ease: 'easeOut'
+            }
+
+        },
+    }
+    const tagline = {
+        initial: {
+            opacity: 0,
+            x: 30,
+        },
+        animate: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                duration: 4,
+                ease: 'easeOut'
+            }
+
+        },
+    }
+    const box = {
+        initial: {
+
+            y: 0,
+        },
+        animate: {
+            y: '-100vh',
+            transition: {
+                delay: 1,
+                duration: 1,
+                ease: 'easeInOut'
+            }
+
+        },
+
+    }
+    const heroimage = {
+        initial: {
+
+            scale: 1.3
+        },
+        animate: {
+            scale: 1,
+            transition: {
+                delay: 1.5,
+                duration: 3,
+                ease: 'anticipate'
+
+            }
+
+        },
+
+    }
+    const nav = {
+        initial: {
+
+            opacity: 0
+        },
+        animate: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.5,
+                delay: 1.5,
+                duration: 1,
+                ease: 'easeInOut'
+
+            }
+
+        },
+
+    }
+
+
     return (
-        <main className="w-screen h-screen
-        flex items-center relative">
-            <div className={herostyles.headline}>
+        <main className="h-screen flex-col
+        flex justify-center items-start relative overflow-x-hidden">
+            <motion.div className={herostyles.headline}
+                variants={headline}
+                initial='initial'
+                animate='animate'
+            >
                 Michaels & Partners
-            </div >
-            <div className={herostyles.heroimage}>
-            </div>
-            <div className={herostyles.box}>
-            </div>
+            </motion.div >
+            <motion.div className={herostyles.tagline}
+                variants={tagline}
+                initial='initial'
+                animate='animate'
+            >
+                Real Estate
+            </motion.div >
+            <motion.div className={herostyles.nav}
+                variants={nav}
+                initial='initial'
+                animate='animate'
+            >
+                <li>houses</li>
+                <li>about</li>
+                <li>contact</li>
+            </motion.div >
+            <motion.div className={herostyles.heroimage}
+                variants={heroimage}
+                initial='initial'
+                animate='animate'
+
+            >
+            </motion.div>
+            <motion.div className={herostyles.box}
+                variants={box}
+                initial='initial'
+                animate='animate'
+            >
+            </motion.div>
         </main>
     )
 }
+
